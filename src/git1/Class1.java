6f44551b7c53647638063af9b0a06ac4419887fd
dharmagitacc <draj371@gmail.com>
2022-08-29 12:45:11 +0530

    test

---
+++ src/git1/Class1.java
@@ -0,0 +1,15 @@
+package git1;
+
+public class Class1 {
+private void method() {
+System.out.println("system");
+}
+private void test() {
+System.out.println("test");
+}
+public static void main(String[] args) {
+	Class1 c = new Class1();
+	c.method();
+	c.test();
+}
+}
